b608365150b8a356319058ffaa2aac0758251c45
KARABERNOUmohamedislem <im_karabernou@esi.dz>
2023-01-14 15:08:16 +0100

    integrated the pldi model

--- tutorials/tutorial_autoscheduler/model/main.py
+++ tutorials/tutorial_autoscheduler/model/main.py
@@ -12,13 +12,6 @@ import time
 
 import os
 
-environ["MKL_THREADING_LAYER"] = "GNU"
-logging.basicConfig(filename="log_new_model.txt", filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='%H:%M:%S',
-                    level=logging.DEBUG)
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-warnings.filterwarnings("ignore", category=UserWarning)
 
 
 # model_path = '/data/scratch/mmerouani/tiramisu2/tiramisu/tutorials/tutorial_autoscheduler/model/multi_model_all_data_12+4+1.3.pkl'
@@ -27,21 +20,21 @@ warnings.filterwarnings("ignore", category=UserWarning)
 # model_path = '/data/scratch/mmerouani/tiramisu2/tiramisu/tutorials/tutorial_autoscheduler/model/MAPE_base_13+4+2.6_22.7.pkl'
 # model_path = '/data/scratch/hbenyamina/best_model_bidirectional_new_data_static_input_paper_4cb2.pt'
 model_path = (
-    "/home/islem/pfe/tiramisu_work/tiramisu/tutorials/tutorial_autoscheduler/model/best_model_bidirectional_static_input_nn_bd67.pt"
+    "/home/islem/pfe/tiramisu_work/tiramisu/tutorials/tutorial_autoscheduler/model/best_model_multiple_roots_fc3e.pt"
 )
 
 MAX_DEPTH = 5
 
 def seperate_vector(
-    X: torch.Tensor, num_matrices: int = 4, pad: bool = True, pad_amount: int = 5
+    X: torch.Tensor, num_matrices: int = 5, pad: bool = True, pad_amount: int = 5
 ) -> torch.Tensor:
     batch_size, _ = X.shape
-    first_part = X[:, :33]
-    second_part = X[:, 33 : 33 + 169 * num_matrices]
-    third_part = X[:, 33 + 169 * num_matrices :]
+    first_part = X[:, :28]
+    second_part = X[:, 28 : 28 + 36 * num_matrices]
+    third_part = X[:, 28 + 36 * num_matrices :]
     vectors = []
     for i in range(num_matrices):
-        vector = second_part[:, 169 * i : 169 * (i + 1)].reshape(batch_size, 1, -1)
+        vector = second_part[:, 36 * i : 36 * (i + 1)].reshape(batch_size, 1, -1)
         vectors.append(vector)
 
     if pad:
@@ -57,7 +50,7 @@ with torch.no_grad():
     environ["layers"] = "600 350 200 180"
     environ["dropouts"] = "0.05 " * 4
     logging.info("got here")
-    input_size = 1056
+    input_size = 1552 #1056
     output_size = 1
 
     layers_sizes = list(map(int, environ.get("layers", "600 350 200 180").split()))
@@ -67,7 +60,7 @@ with torch.no_grad():
         input_size=input_size,
         comp_embed_layer_sizes=layers_sizes,
         drops=drops,
-        transformation_matrix_dimension=13,
+        transformation_matrix_dimension=6,
         loops_tensor_size=33,
         expr_embed_size=100,
         train_device=device,
@@ -83,10 +76,19 @@ with torch.no_grad():
             while True:
                 
                 
-                prog_json = input()
-                no_sched_json = input()
-                sched_json = input()
-
+                # prog_json = input()
+                # no_sched_json = input()
+                # sched_json = input()
+                prog_json = """
+                {"memory_size" : "0.088989" ,"iterators" : {"jmean_init0" : {"lower_bound" : 0,"upper_bound" : 28,"parent_iterator" : null,"child_iterators" : [],"computations_list" : ["mean_init"]},"lmean0" : {"lower_bound" : 0,"upper_bound" : 32,"parent_iterator" : null,"child_iterators" : ["jmean1"],"computations_list" : []},"jmean1" : {"lower_bound" : 0,"upper_bound" : 28,"parent_iterator" : "lmean0","child_iterators" : [],"computations_list" : ["mean"]},"iconv_init0" : {"lower_bound" : 0,"upper_bound" : 28,"parent_iterator" : null,"child_iterators" : ["jconv_init1"],"computations_list" : []},"jconv_init1" : {"lower_bound" : 0,"upper_bound" : 28,"parent_iterator" : "iconv_init0","child_iterators" : [],"computations_list" : ["conv_init"]},"icov0" : {"lower_bound" : 0,"upper_bound" : 28,"parent_iterator" : null,"child_iterators" : ["jcov1"],"computations_list" : []},"jcov1" : {"lower_bound" : 0,"upper_bound" : 28,"parent_iterator" : "icov0","child_iterators" : ["kcov2"],"computations_list" : []},"kcov2" : {"lower_bound" : 0,"upper_bound" : 32,"parent_iterator" : "jcov1","child_iterators" : [],"computations_list" : ["cov"]}},"computations" : {"mean_init" : {"absolute_order" : 1,"iterators" : ["jmean_init0"],"comp_is_reduction" : false,"number_of_additions" : 0,"number_of_subtraction" : 0,"number_of_multiplication" : 0,"number_of_division" : 0,"write_access_relation" : "{ mean_init[j] -> b_mean[j' = j] }","write_buffer_id" : 1,"data_type" : "float64","data_type_size" : 0,"accesses" : [],"expression_representation" : {"expr_type" :  "value",  "children" : []}},"mean" : {"absolute_order" : 2,"iterators" : ["lmean0","jmean1"],"comp_is_reduction" : true,"number_of_additions" : 1,"number_of_subtraction" : 0,"number_of_multiplication" : 0,"number_of_division" : 1,"write_access_relation" : "{ mean[l, j] -> b_mean[j] }","write_buffer_id" : 1,"data_type" : "float64","data_type_size" : 0,"accesses" : [{"access_is_reduction" : true,"buffer_id" : 1,"access_matrix" : [[1, 0, 0],[0, 1, 0]]},{"access_is_reduction" : false,"buffer_id" : 0,"access_matrix" : [[1, 0, 0],[0, 1, 0]]}],"expression_representation" : {"expr_type" :  "add",  "children" : [{"expr_type" :  "access",  "children" : []},{"expr_type" :  "div",  "children" : [{"expr_type" :  "access",  "children" : []},{"expr_type" :  "cast",  "children" : [{"expr_type" :  "value",  "children" : []}]}]}]}},"conv_init" : {"absolute_order" : 3,"iterators" : ["iconv_init0","jconv_init1"],"comp_is_reduction" : false,"number_of_additions" : 0,"number_of_subtraction" : 0,"number_of_multiplication" : 0,"number_of_division" : 0,"write_access_relation" : "{ conv_init[i, j] -> b_cov[i' = i, j' = j] }","write_buffer_id" : 2,"data_type" : "float64","data_type_size" : 0,"accesses" : [],"expression_representation" : {"expr_type" :  "value",  "children" : []}},"cov" : {"absolute_order" : 4,"iterators" : ["icov0","jcov1","kcov2"],"comp_is_reduction" : true,"number_of_additions" : 1,"number_of_subtraction" : 2,"number_of_multiplication" : 1,"number_of_division" : 1,"write_access_relation" : "{ cov[i, j, k] -> b_cov[i, j] }","write_buffer_id" : 2,"data_type" : "float64","data_type_size" : 0,"accesses" : [{"access_is_reduction" : true,"buffer_id" : 2,"access_matrix" : [[1, 0, 0, 0],[0, 1, 0, 0],[0, 0, 1, 0]]},{"access_is_reduction" : false,"buffer_id" : 0,"access_matrix" : [[0, 0, 1, 0],[1, 0, 0, 0]]},{"access_is_reduction" : false,"buffer_id" : 1,"access_matrix" : [[0, 0, 0, 0],[1, 0, 0, 0]]},{"access_is_reduction" : false,"buffer_id" : 0,"access_matrix" : [[0, 0, 1, 0],[0, 1, 0, 0]]},{"access_is_reduction" : false,"buffer_id" : 1,"access_matrix" : [[0, 0, 0, 0],[0, 1, 0, 0]]}],"expression_representation" : {"expr_type" :  "add",  "children" : [{"expr_type" :  "access",  "children" : []},{"expr_type" :  "div",  "children" : [{"expr_type" :  "mul",  "children" : [{"expr_type" :  "sub",  "children" : [{"expr_type" :  "access",  "children" : []},{"expr_type" :  "access",  "children" : []}]},{"expr_type" :  "sub",  "children" : [{"expr_type" :  "access",  "children" : []},{"expr_type" :  "access",  "children" : []}]}]},{"expr_type" :  "cast",  "children" : [{"expr_type" :  "value",  "children" : []}]}]}]}}}}
+                """
+                no_sched_json = """
+                {"mean_init" : {"shiftings" : null,"interchange_dims" : [],"transformation_matrices" : [],"transformation_matrix" : [],"tiling" : {},"unrolling_factor" : null,"parallelized_dim" : null, "skewing" : null}, "mean" : {"shiftings" : null,"interchange_dims" : [],"transformation_matrices" : [],"transformation_matrix" : [],"tiling" : {},"unrolling_factor" : null,"parallelized_dim" : null, "skewing" : null}, "conv_init" : {"shiftings" : null,"interchange_dims" : [],"transformation_matrices" : [],"transformation_matrix" : [],"tiling" : {},"unrolling_factor" : null,"parallelized_dim" : null, "skewing" : null}, "cov" : {"shiftings" : null,"interchange_dims" : [],"transformation_matrices" : [],"transformation_matrix" : [],"tiling" : {},"unrolling_factor" : null,"parallelized_dim" : null, "skewing" : null}, "fusions" : null, "sched_str": "", "tree_structure": {"roots" : [{"loop_name" : "jmean_init0","computations_list" : ["mean_init"],"child_list" : []},{"loop_name" : "lmean0","computations_list" : [],"child_list" : [{"loop_name" : "jmean1","computations_list" : ["mean"],"child_list" : []}]},{"loop_name" : "iconv_init0","computations_list" : [],"child_list" : [{"loop_name" : "jconv_init1","computations_list" : ["conv_init"],"child_list" : []}]},{"loop_name" : "icov0","computations_list" : [],"child_list" : [{"loop_name" : "jcov1","computations_list" : [],"child_list" : [{"loop_name" : "kcov2","computations_list" : ["cov"],"child_list" : []}]}]}]}}
+                """
+                sched_json = """
+                {"mean_init" : {"shiftings" : null,"interchange_dims" : [],"transformation_matrices" : [["1"] ],"transformation_matrix" : ["1"],"tiling" : {},"unrolling_factor" : null,"parallelized_dim" : null, "skewing" : null}, "mean" : {"shiftings" : null,"interchange_dims" : [],"transformation_matrices" : [["1", "0", "0", "1"] , ["1", "0", "0", "-1"] ],"transformation_matrix" : ["1", "0", "0", "-1"],"tiling" : {},"unrolling_factor" : null,"parallelized_dim" : null, "skewing" : null}, "conv_init" : {"shiftings" : null,"interchange_dims" : [],"transformation_matrices" : [["1", "0", "0", "1"] ],"transformation_matrix" : ["1", "0", "0", "1"],"tiling" : {},"unrolling_factor" : null,"parallelized_dim" : null, "skewing" : null}, "cov" : {"shiftings" : null,"interchange_dims" : [],"transformation_matrices" : [["1", "0", "0", "0", "1", "0", "0", "0", "1"] ],"transformation_matrix" : ["1", "0", "0", "0", "1", "0", "0", "0", "1"],"tiling" : {},"unrolling_factor" : null,"parallelized_dim" : null, "skewing" : null}, "fusions" : null, "sched_str": "M({C0},1)M({C1},1,0,0,-1)M({C2},1,0,0,1)M({C3},1,0,0,0,1,0,0,0,1)", "tree_structure": {"roots" : [{"loop_name" : "jmean_init0","computations_list" : ["mean_init"],"child_list" : []},{"loop_name" : "lmean0","computations_list" : [],"child_list" : [{"loop_name" : "jmean1","computations_list" : ["mean"],"child_list" : []}]},{"loop_name" : "iconv_init0","computations_list" : [],"child_list" : [{"loop_name" : "jconv_init1","computations_list" : ["conv_init"],"child_list" : []}]},{"loop_name" : "icov0","computations_list" : [],"child_list" : [{"loop_name" : "jcov1","computations_list" : [],"child_list" : [{"loop_name" : "kcov2","computations_list" : ["cov"],"child_list" : []}]}]}]}}
+                """
+                
                 program_json = json.loads(prog_json)
                 sched_json = json.loads(sched_json)
                 no_sched_json = json.loads(no_sched_json)
@@ -114,7 +116,7 @@ with torch.no_grad():
                 batch_size, num_comps, __dict__ = x.shape
                 x = x.view(batch_size * num_comps, -1)
                 (first_part, final_matrix, vectors, third_part) = seperate_vector(
-                        x, num_matrices=5, pad=False
+                        x, num_matrices=6, pad=False
                     )
                 x = torch.cat(
                     (
